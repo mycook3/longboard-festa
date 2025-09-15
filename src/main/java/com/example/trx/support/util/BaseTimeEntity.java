f8c09b7bb057db2df6e2e3aa678a9d594ae3de31
sgkim <kimsg64090@gmail.com>
2025-09-15 15:36:00 +0900

    feat: BaseTimeEntity 생성

---
+++ src/main/java/com/example/trx/support/util/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.trx.support.util;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)  // 이벤트 리스너
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+}
