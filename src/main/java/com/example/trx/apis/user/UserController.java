3a60b55eec7b4a0ddd447009cd91d1ebcd89a62a
sgkim <kimsg64090@gmail.com>
2025-09-16 23:03:24 +0900

    fix: UserController 비활성화

--- src/main/java/com/example/trx/apis/user/UserController.java
+++ src/main/java/com/example/trx/apis/user/UserController.java
@@ -18,28 +18,28 @@ public class UserController {
 
   private final UserService userService;
 
-  @Operation(summary = "List users", description = "Retrieve all registered users")
-  @GetMapping
-  public ApiResult<List<UserDto>> getUsers() {
-    return ApiResult.succeed(userService.getAllUsers());
-  }
-
-  @Operation(summary = "Get user", description = "Retrieve detailed information for a single user")
-  @GetMapping("/{userId}")
-  public ApiResult<UserDto> getUser(@PathVariable Long userId) {
-    return ApiResult.succeed(userService.getUser(userId));
-  }
-
-  @Operation(summary = "Create user", description = "Register a new user")
-  @PostMapping
-  public ApiResult<UserDto> createUser(@RequestBody UserDto dto) {
-    return ApiResult.succeed(userService.createUser(dto));
-  }
-
-  @Operation(summary = "Update user", description = "Update information of an existing user")
-  @PutMapping("/{userId}")
-  public ApiResult<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto dto) {
-    return ApiResult.succeed(userService.updateUser(userId, dto));
-  }
+//  @Operation(summary = "List users", description = "Retrieve all registered users")
+//  @GetMapping
+//  public ApiResult<List<UserDto>> getUsers() {
+//    return ApiResult.succeed(userService.getAllUsers());
+//  }
+//
+//  @Operation(summary = "Get user", description = "Retrieve detailed information for a single user")
+//  @GetMapping("/{userId}")
+//  public ApiResult<UserDto> getUser(@PathVariable Long userId) {
+//    return ApiResult.succeed(userService.getUser(userId));
+//  }
+//
+//  @Operation(summary = "Create user", description = "Register a new user")
+//  @PostMapping
+//  public ApiResult<UserDto> createUser(@RequestBody UserDto dto) {
+//    return ApiResult.succeed(userService.createUser(dto));
+//  }
+//
+//  @Operation(summary = "Update user", description = "Update information of an existing user")
+//  @PutMapping("/{userId}")
+//  public ApiResult<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto dto) {
+//    return ApiResult.succeed(userService.updateUser(userId, dto));
+//  }
 }
 
