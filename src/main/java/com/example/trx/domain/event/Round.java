b6a146ba35ec0706e3e430b79ce1bde7bb254c46
frog-slayer <pj0642@gmail.com>
2025-09-18 16:08:40 +0900

    feat: Round 진행 메서드를 추가 #1

--- src/main/java/com/example/trx/domain/event/Round.java
+++ src/main/java/com/example/trx/domain/event/Round.java
@@ -1,5 +1,10 @@
 package com.example.trx.domain.event;
 
+import com.example.trx.domain.user.Participant;
+import java.util.ArrayList;
+import java.util.List;
+
+//종목별 라운드 진행 상황
 public enum Round {
   PRELIMINARY(1000),//32강 선발
   ROUND_32(32),
@@ -13,5 +18,17 @@ public enum Round {
 
   Round(int i) {
     this.limit = i;
+  }
+
+  public Round proceed() throws IllegalStateException {
+    return switch (this) {
+      case PRELIMINARY -> ROUND_32;
+      case ROUND_32 -> ROUND_16;
+      case ROUND_16 -> ROUND_8;
+      case ROUND_8 -> SEMI_FINAL;
+      case SEMI_FINAL -> FINAL;
+      case FINAL -> WIN;
+      case WIN -> throw new IllegalStateException("종료된 종목입니다");
+    };
   }
 }
