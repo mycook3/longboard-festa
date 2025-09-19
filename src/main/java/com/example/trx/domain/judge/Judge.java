e09a8f24cbdc40786a1875ddb022efbceef7f32d
frog-slayer <pj0642@gmail.com>
2025-09-19 19:43:04 +0900

    feat: Judge에 점수 제출 메서드를 추가 #1

--- src/main/java/com/example/trx/domain/judge/Judge.java
+++ src/main/java/com/example/trx/domain/judge/Judge.java
@@ -1,9 +1,11 @@
 package com.example.trx.domain.judge;
 
 import com.example.trx.domain.event.ContestEvent;
+import com.example.trx.domain.run.Run;
 import com.example.trx.domain.score.ScoreTotal;
 import com.example.trx.support.util.BaseTimeEntity;
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 import java.util.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,9 +35,14 @@ public class Judge extends BaseTimeEntity {
     @Column(name = "name", nullable = false, length = 64)
     private String name;
 
-
     // 관계: Judge 1 : N ScoreTotal
     @OneToMany(mappedBy = "judge", cascade = CascadeType.ALL, orphanRemoval = false)
     @Builder.Default
     private List<ScoreTotal> scores = new ArrayList<>();
+
+    // 채점
+    public void submitScore(Run run, BigDecimal score, String breakDownJson) {
+      ScoreTotal submitted = new ScoreTotal(run, this, score, breakDownJson);
+      scores.add(submitted);
+    }
 }
