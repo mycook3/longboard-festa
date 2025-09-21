ac160e5af63ec2f154b90f04978a12fff7fc8dfd
sgkim <kimsg64090@gmail.com>
2025-09-21 16:12:35 +0900

    feat: 공지 응답 구조 변경 및 페이지네이션 구현

--- src/main/java/com/example/trx/apis/notice/NoticeController.java
+++ src/main/java/com/example/trx/apis/notice/NoticeController.java
@@ -2,18 +2,19 @@ package com.example.trx.apis.notice;
 
 import com.example.trx.apis.dto.ApiResult;
 import com.example.trx.apis.notice.dto.NoticeCreateRequest;
+import com.example.trx.apis.notice.dto.NoticeListResponse;
 import com.example.trx.apis.notice.dto.NoticeResponse;
 import com.example.trx.service.notice.NoticeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +33,12 @@ public class NoticeController {
         return ApiResult.succeed(noticeService.createNotice(request));
     }
 
-    @Operation(summary = "공지 조회", description = "적용 시각이 지난 공지를 상단 고정 순으로 조회합니다.")
+    @Operation(summary = "공지 조회", description = "상단 고정 공지와 일반 공지를 분리해 조회합니다.")
     @GetMapping
-    public ApiResult<List<NoticeResponse>> getNotices() {
-        return ApiResult.succeed(noticeService.getNotices());
+    public ApiResult<NoticeListResponse> getNotices(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return ApiResult.succeed(noticeService.getNotices(page, size));
     }
 }
