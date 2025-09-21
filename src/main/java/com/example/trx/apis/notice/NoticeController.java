a659460ec16e62f0c41bee1cd3eb460e32332309
sgkim <kimsg64090@gmail.com>
2025-09-21 16:20:47 +0900

    fix: endpoint 컨벤션에 맞게 수정

--- src/main/java/com/example/trx/apis/notice/NoticeController.java
+++ src/main/java/com/example/trx/apis/notice/NoticeController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/notices")
+@RequestMapping("/api/v1/notices")
 @RequiredArgsConstructor
 @Tag(name = "Notice", description = "공지 관리 API")
 public class NoticeController {
