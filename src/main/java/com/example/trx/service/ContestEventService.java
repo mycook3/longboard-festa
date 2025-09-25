a255896df73550224402803e088f284fa06de1e8
frog-slayer <pj0642@gmail.com>
2025-09-25 17:41:26 +0900

    feat: DisciplineCode & Division을 통한 ContestEvent 조회 메서드 추가

--- src/main/java/com/example/trx/service/ContestEventService.java
+++ src/main/java/com/example/trx/service/ContestEventService.java
@@ -4,6 +4,7 @@ import com.example.trx.domain.event.ContestEvent;
 import com.example.trx.domain.event.DisciplineCode;
 import com.example.trx.domain.event.Division;
 import com.example.trx.repository.ContestEventRepository;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,12 @@ import org.springframework.stereotype.Service;
 public class ContestEventService {
 
   private final ContestEventRepository contestEventRepository;
+
+  public ContestEvent getContestEventByDivisionAndDisciplineCode(Division division, DisciplineCode disciplineCode) throws NoSuchElementException {
+    return contestEventRepository
+        .findContestEventByDivisionAndDisciplineCode(division, disciplineCode)
+        .orElseThrow(() -> new NoSuchElementException("Contest event not found"));
+  }
 
   //종목별 참가자 추가
   public void addParticipant() {
