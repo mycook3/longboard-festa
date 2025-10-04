db65e10bacbec2aacc963c6c89cd22224b5f6cc5
frog-slayer <pj0642@gmail.com>
2025-10-04 22:22:12 +0900

    fix: Security에 /api/api/를 추가. application.yaml의 charser 오타 -> charset으로 변경

--- src/main/java/com/example/trx/service/event/ContestEventService.java
+++ src/main/java/com/example/trx/service/event/ContestEventService.java
@@ -5,7 +5,12 @@ import com.example.trx.domain.event.DisciplineCode;
 import com.example.trx.domain.event.Division;
 import com.example.trx.domain.event.Round;
 import com.example.trx.domain.event.exception.ContestEventNotFound;
+import com.example.trx.domain.user.Participant;
+import com.example.trx.domain.user.Participation;
+import com.example.trx.domain.user.ParticipationStatus;
 import com.example.trx.repository.event.ContestEventRepository;
+import com.example.trx.repository.user.ParticipationRepository;
+import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ContestEventService {
 
   private final ContestEventRepository contestEventRepository;
+  private final ParticipationRepository participationRepository;
 
   public ContestEvent getContestEventByDivisionAndDisciplineCode(String divisionName, String eventName) {
     Division division = Division.valueOf(divisionName);
@@ -27,7 +33,7 @@ public class ContestEventService {
   }
 
   @Transactional
-  public ContestEvent createContestEvent(String divisionName, String eventName) {//TODO: 응답 DTO
+  public ContestEvent createContestEvent(String divisionName, String eventName) {//TODO: 응답 DTO로 변경하기
     Division division = Division.valueOf(divisionName);
     DisciplineCode disciplineCode = DisciplineCode.valueOf(eventName);
 
@@ -37,6 +43,30 @@ public class ContestEventService {
         .build();
 
     return contestEventRepository.save(contestEvent);
+  }
+
+  @Transactional
+  public void setRound(Long eventId, String roundName) {
+    ContestEvent contestEvent = contestEventRepository.findById(eventId)
+        .orElseThrow(() -> new ContestEventNotFound(eventId));
+
+    Round round = Round.valueOf(roundName);
+    contestEvent.setRound(round);
+  }
+
+  @Transactional
+  public void addRuns(Long eventId) {
+    ContestEvent contestEvent = contestEventRepository.findById(eventId)
+        .orElseThrow(() -> new ContestEventNotFound(eventId));
+
+    List<Participant> activeParticipants = participationRepository.findByContestEventAndStatus(contestEvent, ParticipationStatus.ACTIVE)
+        .stream()
+        .map(Participation::getParticipant)
+        .toList();
+
+    for (Participant participant : activeParticipants) {
+      contestEvent.addRun(participant);
+    }
   }
 
   @Transactional
@@ -53,14 +83,5 @@ public class ContestEventService {
     } catch (IllegalStateException e) {
       contestEvent.proceedRoundAndDropParticipants();
     }
-  }
-
-  @Transactional
-  public void setRound(Long eventId, String roundName) {
-    ContestEvent contestEvent = contestEventRepository.findById(eventId)
-        .orElseThrow(() -> new ContestEventNotFound(eventId));
-
-    Round round = Round.valueOf(roundName);
-    contestEvent.setRound(round);
   }
 }
