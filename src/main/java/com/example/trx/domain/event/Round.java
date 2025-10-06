52af2326782e72d8a19a4b5af666cfe5db8b81b5
frog-slayer <pj0642@gmail.com>
2025-10-06 12:06:44 +0900

    feat: ContestEvent, Round, Judging 등의 엔티티를 재설계

--- src/main/java/com/example/trx/domain/event/Round.java
+++ src/main/java/com/example/trx/domain/event/Round.java
@@ -1,9 +1,12 @@
 package com.example.trx.domain.event;
 
 import com.example.trx.domain.run.Run;
+import com.example.trx.domain.score.ScoreTotal;
 import com.example.trx.domain.user.Participant;
 import com.example.trx.domain.user.UserStatus;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +14,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
@@ -35,6 +40,9 @@ public class Round {
 
   @OneToOne(fetch = FetchType.LAZY)
   private Run currentRun;
+
+  @Enumerated(EnumType.STRING)
+  private RoundStatus status;
 
   @OneToMany
   private List<Run> runs;
@@ -63,5 +71,37 @@ public class Round {
     if (run == null) throw new IllegalArgumentException("Run is null");
     run.markAsOngoing();
     currentRun = run;
+  }
+
+  public void markAsInProgress() {
+    this.status = RoundStatus.IN_PROGRESS;
+  }
+
+  public void markAsCompleted() {
+    this.status = RoundStatus.COMPLETED;
+  }
+
+  public boolean canBeCompleted() {
+    return this.currentRun == runs.get(runs.size() - 1) && this.currentRun.getUserStatus() == UserStatus.DONE;
+  }
+
+  public List<Participant> getSurvivors(Round nextRound) {
+    return runs.stream()
+        .sorted(
+            Comparator.comparing((Run run) -> {
+              List<BigDecimal> scores = run.getScores()
+                  .stream()
+                  .map(ScoreTotal::getTotal)
+                  .sorted()
+                  .toList();
+
+              return scores.subList(1, scores.size() - 1)
+                  .stream()
+                  .reduce(BigDecimal.ZERO, BigDecimal::add);
+            }).reversed()
+        )
+        .limit(nextRound.getLimit())
+        .map(Run::getParticipant)
+        .toList();
   }
 }
---
+++ src/main/java/com/example/trx/domain/event/RoundStatus.java
@@ -0,0 +1,7 @@
+package com.example.trx.domain.event;
+
+public enum RoundStatus {
+  BEFORE,
+  IN_PROGRESS,
+  COMPLETED;
+}
