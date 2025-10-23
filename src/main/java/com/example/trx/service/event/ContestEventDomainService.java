8eff2c1002325ec7ef03568049fa0984283c4cea
jihwan.Park <mycook3@naver.com>
2025-10-23 21:27:21 +0900

    txt: merge 오류 해결

--- src/main/java/com/example/trx/service/event/ContestEventDomainService.java
+++ src/main/java/com/example/trx/service/event/ContestEventDomainService.java
@@ -69,12 +69,6 @@ public class ContestEventDomainService {
   }
 
   @Transactional
-  public List<ContestEvent> getContestEventsRoundInProgress() {
-    List<Round> rounds = roundRepository.findRoundsByStatus(RoundStatus.IN_PROGRESS);
-    return rounds.stream().map(Round::getContestEvent).toList();
-  }
-
-  @Transactional
   public Round addRound(Long eventId, String roundName, Integer limit, Integer runPerParticipant) {
     ContestEvent contestEvent = getContestEventById(eventId);
     return contestEvent.addRound(roundName, limit, runPerParticipant);
