7205107522cef609faa86b9edc8cf0227848bfb3
frog-slayer <pj0642@gmail.com>
2025-10-25 00:09:44 +0900

    chore: 테스트용 dump sql 파일을 위한 application.yml 세팅

--- src/main/java/com/example/trx/apis/event/dto/request/SubmitScoreRequest.java
+++ src/main/java/com/example/trx/apis/event/dto/request/SubmitScoreRequest.java
@@ -3,9 +3,13 @@ package com.example.trx.apis.event.dto.request;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SubmitScoreRequest {
   private Long judgeId;
   private BigDecimal scoreTotal;
