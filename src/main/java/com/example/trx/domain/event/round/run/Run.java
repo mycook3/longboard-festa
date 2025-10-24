cd49f2a253bf1a225997b10491ccd4d926dd2ff7
frog-slayer <pj0642@gmail.com>
2025-10-25 07:43:36 +0900

    feat: Slalom을 위한 계산 로직 수정

--- src/main/java/com/example/trx/domain/event/round/run/Run.java
+++ src/main/java/com/example/trx/domain/event/round/run/Run.java
@@ -8,6 +8,7 @@ import com.example.trx.domain.user.Participant;
 import com.example.trx.support.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.*;
 import lombok.AllArgsConstructor;
@@ -101,10 +102,10 @@ public class Run extends BaseTimeEntity {
       if (scores.size() > 2) {//3인 이상. 최고점/최저점을 제외
         List<BigDecimal> middleScores = scores.subList(1, scores.size() - 1);
         BigDecimal sum = middleScores.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-        return sum.divide(BigDecimal.valueOf(middleScores.size()), 2, RoundingMode.HALF_UP);
+        return sum.divide(BigDecimal.valueOf(middleScores.size()), 2, RoundingMode.HALF_UP).add(record).subtract(BigDecimal.valueOf(0.5 * touch));
       }
 
       BigDecimal sum = scores.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-      return sum.divide(BigDecimal.valueOf(scores.size()), 2, RoundingMode.HALF_UP);
+      return sum.divide(BigDecimal.valueOf(scores.size()), 2, RoundingMode.HALF_UP).add(record).subtract(BigDecimal.valueOf(0.5 * touch));
     }
 }
